b43f7544409a885e465874b545ca75f4aa1c8337
alperadem <54avci1907@gmail.com>
2021-09-06 14:54:01 +0300

    Mini RPG anvil game repository

---
+++ index.js
@@ -0,0 +1,11 @@
+//SETUP
+const anvilButton = document.getElementById("UPGRADEbutton");
+
+//CODE
+
+    //button
+    anvilButton.addEventListener("click", upgrade =()=>{
+        //button function here
+    });
+    
+    //display
